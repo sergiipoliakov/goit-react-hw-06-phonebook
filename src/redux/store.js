062c55d1d745e5c95c337349893f680fd4676626
sergiipoliakov <64687462+sergiipoliakov@users.noreply.github.com>
2021-03-01 22:08:44 +0200

    добави persistConfic

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,42 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
-
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import phoneBookReducer from './phoneBook/phoneBook-reducer';
 
-const middleware = [...getDefaultMiddleware(), logger];
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  logger,
+];
+
+const phoneBookpersistConfig = {
+  key: 'contacts',
+  storage,
+  blacklist: ['filter'],
+};
+
+// const middleware = [...getDefaultMiddleware(), logger];
 
 const store = configureStore({
   reducer: {
-    phoneBook: phoneBookReducer,
+    phoneBook: persistReducer(phoneBookpersistConfig, phoneBookReducer),
   },
   middleware,
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export default { store, persistor };
