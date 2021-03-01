be4d2005bc166888cbd514733d7359c39d36f533
sergiipoliakov <64687462+sergiipoliakov@users.noreply.github.com>
2021-03-01 18:04:25 +0200

    включил redux-devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,22 +1,15 @@
-// import { combineReducers } from 'redux';
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
-// import { composeWithDevTools } from 'redux-devtools-extension';
+
 import phoneBookReducer from './phoneBook/phoneBook-reducer';
 
 const middleware = [...getDefaultMiddleware(), logger];
 
-// const rootReducer = combineReducers({
-//   phoneBook: phoneBookReducer,
-// });
-
-// const store = createStore(rootReducer, composeWithDevTools());
 const store = configureStore({
   reducer: {
     phoneBook: phoneBookReducer,
   },
   middleware,
-  devTools: process.env.NODE_ENV === 'development',
 });
 
 export default store;
